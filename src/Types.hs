c205aa2b9f463ca8f14c1a8d303b30a4b51ece60
Aleksandr Novikov <mem-memov@yandex.ru>
2021-02-03 09:12:05 +0300

    graph types

---
+++ src/Types.hs
@@ -0,0 +1,12 @@
+data Entry = Entry PositivePointer NegativePointer
+
+newtype PositivePointer = PositivePointer Pointer
+newtype NegativePointer = NegativePointer Pointer
+
+data Pointer = Pointer NodeLink BackwardLink ForwardLink
+
+newtype NodeLink = NodeLink Link
+newtype BackwardLink = BackwardLink Link
+newtype ForwardLink = ForwardLink Link
+
+newtype Link = Link Words
