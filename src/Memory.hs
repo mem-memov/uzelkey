28c5219c2e988ab81add1479fca2b5d51e5a36ce
Александр Новиков <anovikov@tsum.ru>
2021-02-05 19:59:16 +0300

    unsafe read

--- src/Memory.hs
+++ src/Memory.hs
@@ -1,5 +1,8 @@
 module Memory (create) where
 
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+import Data.Array.Base
 import Data.Array.ST 
 import Data.Array.Unboxed
 import Control.Monad 
@@ -9,55 +12,60 @@ import Converter (word8ListToWord, wordToWord8List)
 import Data.List.Split (chunksOf)
 import Data.Maybe (catMaybes, isJust, fromJust)
 
-newtype ByteStorage = ByteStorage (UArray Word Word8)
+type ByteStorage s = ST s (STUArray s Word Word8)
 
-create :: Word -> ByteStorage
-create lastIndex = ByteStorage $ runSTUArray $ do
+create :: Word -> ST s (STUArray s Word Word)
+create lastIndex = do
     stuArray <- newArray (minBound :: Word, lastIndex) 0
     return stuArray
 
-read :: ByteStorage -> Word -> Word -> Word -> Maybe [Word]
-read (ByteStorage uArray) firstIndex chunkLength chunkNumber = 
-    if isOutOfBounds || word8ListLength <= 0
-        then 
-            Nothing
-        else
-            Just wordList
-    where
-        word8ListLength = chunkLength * chunkNumber
-        lastIndex = firstIndex + word8ListLength - 1
+read :: ST s (STUArray s Word Word) -> Word -> ST s (STUArray s Word Word)
+read byteStorage i = byteStorage >>= \stuArray -> do 
+    let val = (unsafeRead stuArray 1) :: (ST s Word)
+    return stuArray
 
-        (minIndex, maxIndex) = bounds uArray
-        isOutOfBounds = firstIndex < fromIntegral minIndex || lastIndex > fromIntegral maxIndex
+-- read :: ByteStorage -> Word -> Word -> Word -> Maybe [Word]
+-- read (ByteStorage uArray) firstIndex chunkLength chunkNumber = 
+--     if isOutOfBounds || word8ListLength <= 0
+--         then 
+--             Nothing
+--         else
+--             Just wordList
+--     where
+--         word8ListLength = chunkLength * chunkNumber
+--         lastIndex = firstIndex + word8ListLength - 1
 
-        word8List = fmap (uArray !) [firstIndex .. lastIndex]
-        wordList = (catMaybes . (map word8ListToWord) . (chunksOf (fromIntegral chunkNumber))) word8List
+--         (minIndex, maxIndex) = bounds uArray
+--         isOutOfBounds = firstIndex < fromIntegral minIndex || lastIndex > fromIntegral maxIndex
+
+--         word8List = fmap (uArray !) [firstIndex .. lastIndex]
+--         wordList = (catMaybes . (map word8ListToWord) . (chunksOf (fromIntegral chunkNumber))) word8List
 
 
-write :: ByteStorage -> Word -> Word -> [Word] -> Maybe ByteStorage
-write (ByteStorage uArray) firstIndex chunkLength wordList = 
-    if isOutOfBounds && chunksOk
-        then 
-            Nothing
-        else
-            Just (ByteStorage updatedUArray)
-    where
-        wordListLength = (fromIntegral (length wordList)) :: Word
-        word8ListLength = chunkLength * wordListLength
-        lastIndex = firstIndex + word8ListLength
-        (minIndex, maxIndex) = bounds uArray
-        isOutOfBounds = firstIndex < fromIntegral minIndex || lastIndex > fromIntegral maxIndex
-        word8ListChunks = map (wordToWord8List chunkLength) wordList
-        chunksOk = all isJust word8ListChunks
-        chunks = map fromJust word8ListChunks
-        updatedUArray = runSTUArray $ do
-            stArray <- thaw uArray
-            let end = (fromIntegral ((length chunks) -1) :: Word)
-            forM_ [0 .. end] $ \i -> do
-                let word8List = chunks !! (fromIntegral i)
-                let end = (fromIntegral ((length word8List) -1) :: Word)
-                forM_ [0 .. end] $ \j -> do
-                    let index = (firstIndex + i * j)
-                    let word8 = word8List !! (fromIntegral j)
-                    writeArray stArray index word8
-            return stArray
+-- write :: ByteStorage -> Word -> Word -> [Word] -> Maybe ByteStorage
+-- write (ByteStorage uArray) firstIndex chunkLength wordList = 
+--     if isOutOfBounds && chunksOk
+--         then 
+--             Nothing
+--         else
+--             Just (ByteStorage updatedUArray)
+--     where
+--         wordListLength = (fromIntegral (length wordList)) :: Word
+--         word8ListLength = chunkLength * wordListLength
+--         lastIndex = firstIndex + word8ListLength
+--         (minIndex, maxIndex) = bounds uArray
+--         isOutOfBounds = firstIndex < fromIntegral minIndex || lastIndex > fromIntegral maxIndex
+--         word8ListChunks = map (wordToWord8List chunkLength) wordList
+--         chunksOk = all isJust word8ListChunks
+--         chunks = map fromJust word8ListChunks
+--         updatedUArray = runSTUArray $ do
+--             stArray <- thaw uArray
+--             let end = (fromIntegral ((length chunks) -1) :: Word)
+--             forM_ [0 .. end] $ \i -> do
+--                 let word8List = chunks !! (fromIntegral i)
+--                 let end = (fromIntegral ((length word8List) -1) :: Word)
+--                 forM_ [0 .. end] $ \j -> do
+--                     let index = (firstIndex + i * j)
+--                     let word8 = word8List !! (fromIntegral j)
+--                     writeArray stArray index word8
+--             return stArray
