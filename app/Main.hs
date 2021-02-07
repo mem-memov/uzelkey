ae554046308787b313158f5d2922494aca65499b
Aleksandr Novikov <mem-memov@yandex.ru>
2021-02-07 11:31:36 +0300

    fix

--- app/Main.hs
+++ app/Main.hs
@@ -6,9 +6,9 @@ main :: IO ()
 main = do
     let a = Memory.create 10000000000
     let b = Memory.read a 1000 4 1
-    let (Just a2) = Memory.write a 1000 4 [255]
+    let (Just a2) = Memory.write a 1000 4 [(1 :: Word)]
     let b2 = Memory.read a 1000 4 1
-    let b3 = Memory.read a2 1000 4 1
+    let b3 = Memory.read a2 2000 4 1
     let b4 = Memory.read a 1000 4 1
     putStrLn ("Done! " ++ show b ++ " " ++ show b2 ++ " " ++ show b3 ++ " " ++ show b4)
     a <- getLine
