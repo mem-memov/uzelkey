fd85e5acffed9234ba0b9778092117d01aae51f2
Aleksandr Novikov <mem-memov@yandex.ru>
2021-02-08 09:41:38 +0300

    converter tests

--- test/ConverterSpec.hs
+++ test/ConverterSpec.hs
@@ -3,6 +3,9 @@ module ConverterSpec where
 import Test.Hspec
 import Converter
 import Data.Word
+import Data.Maybe (fromJust)
+
+-- TODO: take machine arity into account
 
 spec :: Spec
 spec = do
@@ -44,5 +47,52 @@ spec = do
     it "converts [1,0,0,0,0,0,0,0] to the power 48 of 2" $ do
       word8ListToWord [1,0,0,0,0,0,0,0] `shouldBe` Just (2 ^ 56)
 
-    it "converts [255,0,0,0,0,0,0,0] to the power 64 of 2" $ do
+    it "converts [255,255,255,255,255,255,255,255] to the power 64 of 2 without 1" $ do
       word8ListToWord [255,255,255,255,255,255,255,255] `shouldBe` Just (2 ^ 64 - 1)
+
+    it "converts [0,0,0,0,7,91,205,21] to number and back" $ do
+      wordToWord8List 8 (fromJust (word8ListToWord [0,0,0,0,7,91,205,21])) `shouldBe` Just [0,0,0,0,7,91,205,21]
+
+  describe "Converter.wordToWord8List" $ do
+
+    it "returns nothing if resulting list of no length" $ do
+      wordToWord8List 0 12345 `shouldBe` Nothing
+
+    it "returns nothing if resulting list exceeds maximum length" $ do
+      wordToWord8List 9 12345 `shouldBe` Nothing
+
+    it "converts 0 to [0]" $ do
+      wordToWord8List 1 0 `shouldBe` Just [0]
+
+    it "converts 0 to [0,0,0,0,0,0,0,0]" $ do
+      wordToWord8List 8 0 `shouldBe` Just [0,0,0,0,0,0,0,0]
+
+    it "converts the power 0 of 2 to [0,0,0,0,0,0,0,1]" $ do
+      wordToWord8List 8 (2 ^ 0) `shouldBe` Just [0,0,0,0,0,0,0,1]
+
+    it "converts the power 8 of 2 to [0,0,0,0,0,0,1,0]" $ do
+      wordToWord8List 8 (2 ^ 8) `shouldBe` Just [0,0,0,0,0,0,1,0]
+
+    it "converts the power 16 of 2 to [0,0,0,0,0,1,0,0]" $ do
+      wordToWord8List 8 (2 ^ 16) `shouldBe` Just [0,0,0,0,0,1,0,0]
+
+    it "converts the power 24 of 2 to [0,0,0,0,1,0,0,0]" $ do
+      wordToWord8List 8 (2 ^ 24) `shouldBe` Just [0,0,0,0,1,0,0,0]
+
+    it "converts the power 32 of 2 to [0,0,0,1,0,0,0,0]" $ do
+      wordToWord8List 8 (2 ^ 32) `shouldBe` Just [0,0,0,1,0,0,0,0]
+
+    it "converts the power 40 of 2 to [0,0,1,0,0,0,0,0]" $ do
+      wordToWord8List 8 (2 ^ 40) `shouldBe` Just [0,0,1,0,0,0,0,0]
+
+    it "converts the power 48 of 2 to [0,1,0,0,0,0,0,0]" $ do
+      wordToWord8List 8 (2 ^ 48) `shouldBe` Just [0,1,0,0,0,0,0,0]
+
+    it "converts the power 56 of 2 to [1,0,0,0,0,0,0,0]" $ do
+      wordToWord8List 8 (2 ^ 56) `shouldBe` Just [1,0,0,0,0,0,0,0]
+
+    it "converts the power 64 of 2 without 1 to [255,255,255,255,255,255,255,255]" $ do
+      wordToWord8List 8 (2 ^ 64 - 1) `shouldBe` Just [255,255,255,255,255,255,255,255]
+
+    it "converts 123456789 to bytes and back" $ do
+      word8ListToWord (fromJust (wordToWord8List 8 123456789)) `shouldBe` Just 123456789
