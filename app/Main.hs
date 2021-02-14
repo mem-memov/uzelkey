30f5e6105b226e65d29625b87c538b6576e3d75c
Aleksandr Novikov <mem-memov@yandex.ru>
2021-02-14 20:01:03 +0300

    using flip

--- app/Main.hs
+++ app/Main.hs
@@ -10,16 +10,11 @@ main :: IO ()
 main = do
     let entry = do
                     byteStorage <- Memory.create 4 100000000
-                    return $ evalState (
-                            do 
-                                EntryStorage.writeEntry 0 (Serializer.deserialize [1,1,1,1,1,1])
-                                entry <- EntryStorage.readEntry 0
-                                return $ fromJust $ entry
-                        ) byteStorage
-
-    -- let byteStorage_1 = fromJust $ Memory.create 4 100000000
-    -- let byteStorage_2 = execState (EntryStorage.writeEntry 0 (Serializer.deserialize [1,1,1,1,1,1])) byteStorage_1
-    -- let (entry, byteStorage_3) = runState (EntryStorage.readEntry 0) byteStorage_2
+                    entry <- (flip evalState) byteStorage $ do 
+                                                                EntryStorage.writeEntry 0 (Serializer.deserialize [1,1,1,1,1,1])
+                                                                entry <- EntryStorage.readEntry 0
+                                                                return $ entry
+                    return entry
     print entry
     a <- getLine
     if a == "exit" then return () else main 
