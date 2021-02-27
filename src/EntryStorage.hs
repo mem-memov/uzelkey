c5044b5ada5d9d5ef964b78139d466c2bec6924d
Aleksandr Novikov <mem-memov@yandex.ru>
2021-02-27 10:57:11 +0300

    -

--- src/EntryStorage.hs
+++ src/EntryStorage.hs
@@ -1,12 +1,12 @@
 module EntryStorage
-( readEntry
+( Entry(..)
 , readEntry
 , writeEntry ) where
 
 import qualified Memory
 import Control.Monad.State (State, get, put)
 
-data Entry  = Entry [Word] deriving (Eq)
+data Entry = Entry [Word] deriving (Eq, Show)
 
 readEntry :: Int -> State Memory.ChunkStorage (Either String Entry)
 readEntry address = do -- State
@@ -16,9 +16,16 @@ readEntry address = do -- State
                 return (Entry words)
 
 writeEntry :: Int -> Entry -> State Memory.ChunkStorage (Either String ())
-writeEntry address (Entry words) = do -- State
-    storage <- get
-    let modifiedStorage = Memory.write storage address words
-    case modifiedStorage of
-        Left errorMessage -> return (Left errorMessage)
-        Right newStorage -> put newStorage >> return (Right ())
+writeEntry address (Entry words)
+    | length words /= 6 = return $ Left $ "Length of entry should be 6. Length " ++ (show . length) words ++ ". Entry content " ++ (show . take 100) words ++ "."
+    | otherwise = do -- State
+        storage <- get
+        let modifiedStorage = Memory.write storage address words
+        case modifiedStorage of
+            Left errorMessage ->
+                do -- State
+                    return (Left errorMessage)
+            Right newStorage -> 
+                do -- State
+                    put newStorage 
+                    return (Right ())
